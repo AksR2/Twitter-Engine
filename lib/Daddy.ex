ded8a5b5373a267c99e36c7185feca4e5e44bb12
Akshay Rawat <akki.624@gmail.com>
2017-11-19 21:13:48 -0500

    Skeleton code

---
+++ lib/Daddy.ex
@@ -0,0 +1,10 @@
+defmodule Daddy do
+    use Supervisor
+    def start_link(arg) do
+        
+    end
+
+    def init(arg) do
+        
+    end
+end
---
+++ lib/TweeterEngine.ex
@@ -0,0 +1,30 @@
+defmodule TweetEngine do
+    use GenServer
+
+    def init() do
+        
+    end
+
+    def start_link() do
+        
+    end
+
+    def regitserUser() do
+        
+    end
+
+    def sendTweet() do
+        
+    end 
+
+    def subscribeUser() do
+        
+    end
+
+    def queryTweets() do
+    end
+
+    def deliverTweets() do
+        
+    end
+end
