5e45a711748ed2777703819b275743e705fde4b7
Prakhar Mohan Srivastava <prakharmohan@ufl.edu>
2017-11-28 16:57:37 -0500

    Adding some code

--- lib/engine.ex
+++ lib/engine.ex
@@ -9,11 +9,15 @@ defmodule Engine do
         state = %{}
         {_, tweets} = Map.get_and_update(state, :tweets, fn currentVal -> {currentVal, :ets.new(:tweets, [:set, :named_table])} end)
         {_, users} = Map.get_and_update(state, :users, fn currentVal -> {currentVal, :ets.new(:users, [:set, :named_table])} end)
+        {_, hashtags} = Map.get_and_update(state, :hashtags, fn currentVal -> {currentVal, :ets.new(:hashtags, [:duplicate_bag, :named_table])} end)
+        {_, mentions} = Map.get_and_update(state, :mentions, fn currentVal -> {currentVal, :ets.new(:mentions, [:duplicate_bag, :named_table])} end)
         {_, user_id} = Map.get_and_update(state, :user_id, fn currentVal -> {currentVal, 1} end)
         {_, tweet_id} = Map.get_and_update(state, :tweet_id, fn currentVal -> {currentVal, 1} end)
         
         state = Map.merge(state, tweets)
         state = Map.merge(state, users)
+        state = Map.merge(state, hashtags)
+        state = Map.merge(state, mentions)
         state = Map.merge(state, user_id)
         state = Map.merge(state, tweet_id)
         {:ok, state}
@@ -49,14 +53,41 @@ defmodule Engine do
         {:noreply, state}
     end
 
-    def handle_cast({:tweet}, state) do
-        
+    def handle_cast({:tweet, user_id, tweet}, state) do
+        tweet_id = state[:tweet_id]
+        tweets = state[:tweets]
+        hashtags = state[:hashtags]
+        mentions = state[:mentions]
+        list_of_hashtags = Regex.scan(~r/\B#[a-zA-Z0-9_]+/, tweet)
+        list_of_mentions = Regex.scan(~r/\B@[a-zA-Z0-9_]+/, tweet)
+        Enum.each(list_of_hashtags, fn innerList -> {
+            Enum.each(innerList, fn element -> {
+                hashtags = :ets.insert(hashtags, {element, user_id, tweet})
+                {_, hashtags} = Map.get_and_update(state, :hashtags, fn currentVal -> {currentVal, hashtags} end)
+                state = Map.merge(state, hashtags)
+            } end)
+        } end)
+        Enum.each(list_of_mentions, fn innerList -> {
+            Enum.each(innerList, fn element -> {
+                mentions = :ets.insert(mentions, {element, user_id, mentions})
+                {_, mentions} = Map.get_and_update(state, :mentions, fn currentVal -> {currentVal, mentions} end)
+                state = Map.merge(state, mentions)
+            } end)
+        } end)
+        tweets = :ets.insert(tweets, {tweet_id, user_id, tweet})
+        {_, tweet_id} = Map.get_and_update(state, :tweet_id, fn currentVal -> {currentVal, tweet_id + 1} end)
+        state = Map.merge(state, tweet_id)
+        {_, tweets} = Map.get_and_update(state, :tweets, fn currentVal -> {currentVal, tweets} end)
+        state = Map.merge(state, tweets)
+        {:noreply, state}
+
     end
 
-    def handle_cast({:retweet}, state) do
+    def handle_cast({:distribute, user_id}) do
+        users = state[:users]
+        user_info = :ets.lookup(users, user_id)
         
     end
-
 
 
 end
