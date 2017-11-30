dc0a8ccae08856adec0e4f368123bea84fbe402a
Prakhar Mohan Srivastava <prakharmohan@ufl.edu>
2017-11-30 01:55:26 -0500

    Query methods

--- lib/engine.ex
+++ lib/engine.ex
@@ -6,10 +6,10 @@ defmodule Engine do
     end
 
     # table Formats
-    # :user {userid, followers, subscribed}}
+    # :users {userid, followers, subscribed}}
     # :hastags {hashtag, {userid, tweet}}
     # :mentions {mention , {userid, tweet}}
-    # :tweet {user_id, {tweet_id, tweet}}
+    # :tweets {user_id, {tweet_id, tweet}}
 
     def init(:ok) do
         state = %{}
@@ -66,6 +66,7 @@ defmodule Engine do
         tweets = state[:tweets]
         hashtags = state[:hashtags]
         mentions = state[:mentions]
+        users = state[:users]
         list_of_hashtags = Regex.scan(~r/\B#[a-zA-Z0-9_]+/, tweet)
         list_of_mentions = Regex.scan(~r/\B@[a-zA-Z0-9_]+/, tweet)
         Enum.each(list_of_hashtags, fn innerList -> {
@@ -83,17 +84,19 @@ defmodule Engine do
         :ets.insert(tweets, {user_id, {tweet_id,tweet})
         {_, tweet_id} = Map.get_and_update(state, :tweet_id, fn currentVal -> {currentVal, tweet_id + 1} end)
         state = Map.merge(state, tweet_id)
+        user_entry = :ets.lookup(users, user_id_followed)
+        tuple_user_entry = List.first(user_entry)
+        tuple_user_followers = elem(tuple_user_entry, 1)
+        list_followers = Tuple.to_list(tuple_user_followers)
+        Enum.each(list_followers, fn element -> {
+            # should send the tweet to all the followers currently online...
+            #check state of client whether he is online before sending the tweet...
+            #this is the live functionality...
+        } end)
         {:noreply, state}
-
-        # should send the tweet to all the followers currently online...
-        #check state of client whether he is online before sending the tweet...
-        #this is the live functionality...
-
     end
 
     #distribute
-
-
     #fetchtweets when user joins in the network...
     def handle_cast({:fetchtweets, user_id}) do
         users = state[:users]
@@ -103,6 +106,18 @@ defmodule Engine do
         list_of_subscribed= elem(user_enty,1)
         
 
+    end
+
+    def handle_call({:query_hashtag, hashtag}) do
+        hashtags = state[:hastags]
+        hashtags = :ets.lookup(hashtags, hashtag)
+        {:reply, hashtags}
+    end
+
+    def handle_call({:query_mention, mention}) do
+        mentions = state[:mentions]
+        mentions = :ets.lookup(mentions, mention)
+        {:reply, mentions}
     end
 
 
